8fbd03e48b2d4118425958f1c2493a052ce865b8
perthDDD <140643274+perthDDD@users.noreply.github.com>
2023-08-06 02:32:37 +0800

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -7,7 +7,7 @@ loginButton.addEventListener("click", (e) => {
     const username = loginForm.username.value;
     const password = loginForm.password.value;
 
-    if (username === "perthTGW500" && password === "uniteAs1Acheive") {
+    if (username === ${{secrets.USERNAME}} && password === ${{secrets.PASSWORD}}) {
         alert("You have successfully logged in.");
         window.location.href = "https://funky-wave-252.notion.site/TGW-EDU-a5b9146a42804d269e86dcbe55c3f746?pvs=4";
     } else {
